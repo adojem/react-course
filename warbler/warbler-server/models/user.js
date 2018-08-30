35a945f5f337b3c5c1bae0291abfcd8c9366323d
adojem <adojemcovix@gmail.com>
2018-08-30 15:09:16 +0900

    User Middleware

--- warbler/warbler-server/models/user.js
+++ warbler/warbler-server/models/user.js
@@ -24,6 +24,28 @@ const userSchema = new mongoose.Schema({
    }
 });
 
+userSchema.pre('save', async (next) => {
+   try {
+      if (!this.isModified('password')) {
+         return next();
+      }
+      const hashedPassword = await bcrypt.hash(this.password, 10); // bcypt.hash is async
+      this.password = hashedPassword;
+      return next();
+   } catch (err) {
+      return next(err);
+   }
+});
+
+userSchema.method.comparePassword = async (candidatePassword, next) => {
+   try {
+      const isMatch = await bcrypt.compare(candidatePassword, this.password);
+      return isMatch;
+   } catch (err) {
+      return next(err);
+   }
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
