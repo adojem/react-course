bf7c79e9dc4e0406bb71a76c801e56ee64a3c39a
adojem <adojemcovix@gmail.com>
2018-09-01 12:19:45 +0900

    Adding a Navbar

--- warbler/warbler-client/src/containers/App.js
+++ warbler/warbler-client/src/containers/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { configureStore } from '../store';
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = () => (
    <Provider store={store}>
       <Router>
-         <div>Hello World</div>
+         <div className="onboarding">
+            <Navbar />
+         </div>
       </Router>
    </Provider>
 );
