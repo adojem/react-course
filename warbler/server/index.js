83a04ef81fd96a1b28813389859c43f04dbbbb28
adojem <adojemcovix@gmail.com>
2018-12-07 11:51:33 +0900

    Adding auth middleware

--- warbler/server/index.js
+++ warbler/server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+// handlers
 const errorHandler = require('./handlers/error');
-
+// routes
 const authRoutes = require('./routes/auth');
+const messagesRoutes = require('./routes/messages');
+// middleware
+const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
 
 const PORT = 8081;
 
@@ -14,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messagesRoutes);
 
 app.use((req, res, next) => {
    const err = new Error('Not Found');
